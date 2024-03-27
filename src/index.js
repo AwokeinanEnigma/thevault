ed36fd4bb67621f5147b080a203fe1a24e583829
Enigma <49351666+AwokeinanEnigma@users.noreply.github.com>
2024-03-27 10:16:32 -0400

    routing is a pain

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
+
 import NoteLister from "./NoteLister";
 import Why from "./notes/Why";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 const router = createBrowserRouter([
     {
