fdf0b37097e268f92df18a1f816c91303b2a87d7
Enigma <49351666+AwokeinanEnigma@users.noreply.github.com>
2024-03-27 10:07:36 -0400

    experiments

--- src/Global.js
+++ src/Global.js
@@ -7,7 +7,7 @@ export function MakeNavBar() {
         <Navbar expand="sm">
             <Nav className="flex-fill justify-content-center">
                 <Nav.Link href="/thevault" onClick={OnClick} className="nav-link">home</Nav.Link>
-                <Nav.Link href="/thevault/notes" onClick={OnClick} className="nav-link">notes</Nav.Link>
+                <Nav.Link href="/thevault/#/notes" onClick={OnClick} className="nav-link">notes</Nav.Link>
             </Nav>
         </Navbar>
         // i got a glock in my rari
