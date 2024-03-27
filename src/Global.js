9494fabddfe8f5934d606cc11229e5398b6c49ff
Enigma <49351666+AwokeinanEnigma@users.noreply.github.com>
2024-03-27 11:10:14 -0400

    this sucks, routing sucks

--- src/Global.js
+++ src/Global.js
@@ -6,8 +6,8 @@ export function MakeNavBar() {
     return (
         <Navbar expand="sm">
             <Nav className="flex-fill justify-content-center">
-                <Nav.Link to="/thevault" onClick={OnClick} className="nav-link">home</Nav.Link>
-                <Nav.Link to="/thevault/notes" onClick={OnClick} className="nav-link">notes</Nav.Link>
+                <Nav.Link href="/thevault/" onClick={OnClick} className="nav-link">home</Nav.Link>
+                <Nav.Link href="/thevault/notes" to="/thevault/notes" onClick={OnClick} className="nav-link">notes</Nav.Link>
             </Nav>
         </Navbar>
         // i got a glock in my rari
