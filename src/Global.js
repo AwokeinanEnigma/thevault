b33a199ec02243cd90d56bac41b8ac625e4eb3a2
Enigma <49351666+AwokeinanEnigma@users.noreply.github.com>
2024-03-27 10:24:33 -0400

    hopefully fix routing?

--- src/Global.js
+++ src/Global.js
@@ -7,7 +7,7 @@ export function MakeNavBar() {
         <Navbar expand="sm">
             <Nav className="flex-fill justify-content-center">
                 <Nav.Link to="/thevault" onClick={OnClick} className="nav-link">home</Nav.Link>
-                <Nav.Link to="/thevault/#/notes" onClick={OnClick} className="nav-link">notes</Nav.Link>
+                <Nav.Link to="/thevault/notes" onClick={OnClick} className="nav-link">notes</Nav.Link>
             </Nav>
         </Navbar>
         // i got a glock in my rari
