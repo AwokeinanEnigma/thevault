7f63bc46f47ab6c954d483745e301ada654c4240
enigma <49351666+AwokeinanEnigma@users.noreply.github.com>
2024-10-06 23:31:44 -0400

    redo some css add sunless elysium note add stuff page

--- src/Stuff.js
+++ src/Stuff.js
@@ -27,7 +27,8 @@ function MakePage() {
     </div>
 }
 
-const Box = ({ imageSrc, text }) => {
+
+const Box = ({ imageSrc, text, link }) => {
     const [expanded, setExpanded] = useState(false);
     const [isOverflowing, setIsOverflowing] = useState(false);
     const textRef = useRef(null);
@@ -40,7 +41,9 @@ const Box = ({ imageSrc, text }) => {
 
     return (
         <div className="box" onClick={() => setExpanded(!expanded)}>
-            <img src={imageSrc} alt="Box Image" className="box-image" />
+            <a href={link} target="_blank" rel="noopener noreferrer">
+                <img src={imageSrc} alt="Box Image" className="box-image" />
+            </a>
             <div ref={textRef} className={`box-text ${expanded ? 'expanded' : ''}`}>
                 {text}
             </div>
@@ -48,18 +51,40 @@ const Box = ({ imageSrc, text }) => {
     );
 };
 
-
 function MakeMain() {
     const boxes = [
-        { imageSrc: 'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/632360/header.jpg', text: 'This is some text for box 1. It can be very long and should be expandable. God is watching yada yada I mean hell man this is placeholder text like just grow up I guess or get out I dunno dont ask me too many questions shit lorem ispum aint got shit on me if you know what i mean'},
-        { imageSrc: 'image2.png', text: 'This is some text for box 2. It can be very long and should be expandable.' },
-        // Add more boxes as needed
+        {
+            imageSrc: 'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2771670/header.jpg',
+            text: "Psychopomp, as short as it is, is fantastic. I really like this game, it’s dense and packed full of interesting concepts that I haven’t seen in other games. The art style is unique, the gameplay is fun, and the music is great. There’s a song at the end of the game which I still listen to, months after playing. I think you should play it, it’s free and only an hour and a half long. The developer is making a paid, expanded version of the game, which I will 100% pick up on day one.",
+            link: "https://store.steampowered.com/app/2771670/Psychopomp/",
+        },
+        {
+            imageSrc: "firepunchbanner.png",
+            text: "Live.",
+            link: "https://example.com/firepunch",
+        },
+        {
+            imageSrc: "https://static.wixstatic.com/media/b48361_dcc13b3f897f45f3b76eefea596d93c6~mv2.png/v1/fill/w_1920,h_990,al_c,q_90,enc_auto/b48361_dcc13b3f897f45f3b76eefea596d93c6~mv2.png",
+            text: "Murder Drones is such a strange show to write about. It’s so, so, self-indulgent with a lot of its ideas. It feels like the creator had a billion ideas and threw all of them at the wall. Some ideas stuck, some of them... didn’t. It has those rough marks of a creator’s first major production, which are both endearing and annoying at the same time. I can’t say I didn’t enjoy my time with it, but my enjoyment of the show exponentially grew at the last couple of episodes. Not in an ironic “because it was almost over,” sort of way, but because the last few episodes were much better compared to the other episodes. There’s some interesting symbolism with Christianity and God and Satan near the end, but it doesn’t really go anywhere. Not going to spoil anything though. If you’ve got time to kill, it’s worth a watch.  ",
+            link: "?",
+        },
+        {
+            imageSrc: "https://pbs.twimg.com/profile_banners/1403439889052360704/1699673461/1500x500",
+            text: "I'm the writer on this game. 10/10."
+        },
+        {
+            imageSrc: "https://parahumans.wordpress.com/wp-content/uploads/2011/06/cityscape2.jpg",
+            text: "Worm is a two-million word web serial about a girl with bug powers. It's very, very, good. Rough, but interesting, with a lot of interesting ideas. It's good, I recommend it. There's a lot of great world-building, unique characters, and the superpowers are fun and have a lot of interesting interactions. It definitely stumbles towards the end, but there are a few amazing moments here and there. Even though it takes a direction I really dislike towards the end, there are some amazing moments here and there that I still think about, months after reading. Go read it, you'll find it by googling parahumans. It's the wordpress link, not the reddit link."
+        },
+        // todo: add more shit
     ];
 
     return (<div className="container">
 
         <div className="row">
             <div className="col-lg-9 mx-auto">
+                <MakeBigHeader Content="Stuff"/>
+                <MakeTextCentered Content="This is a list of short notes on various games/music/shows that I've played/listened to/watched. There are no ratings here, just thoughts. Also note that none of these are set in stone, so all of these are prone to change at any time. You can click on a box to expand it."/>
                 <div className="container">
                     {boxes.map((box, index) => (
                         <Box key={index} imageSrc={box.imageSrc} text={box.text}/>
@@ -69,25 +94,5 @@ function MakeMain() {
         </div>
     </div>);
 }
-
-const a = () => {
-    const boxes = [
-        {imageSrc: 'image1.png', text: 'This is some text for box 1. It can be very long and should be expandable.'},
-        {imageSrc: 'image2.png', text: 'This is some text for box 2. It can be very long and should be expandable.' },
-        // Add more boxes as needed
-    ];
-
-    return (
-        <div>
-            <MakeNavBar />
-            <div className="container">
-                {boxes.map((box, index) => (
-                    <Box key={index} imageSrc={box.imageSrc} text={box.text} />
-                ))}
-            </div>
-        </div>
-    );
-};
-
 
 export default Stuff
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
 import NoteLister from "./NoteLister";
 import Why from "./notes/Why";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
